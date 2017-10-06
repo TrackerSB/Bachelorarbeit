d5b54e457b4219dd10b0160c9823b7df664cd424
Stefan Huber <junktogo@web.de>
2017-10-06 16:22:06 +0200

    Improve plots

--- r/generatePlotsAndTables.R
+++ r/generatePlotsAndTables.R
@@ -1,3 +1,9 @@
+#Define global configs
+defaultColorSequence <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+#Following should always be added to ensure consistency
+defaultSettings <- scale_fill_manual(values=defaultColorSequence)
+
 #Load and filter data
 library(readr)
 report_ratiosMaxRegions <- read_csv("../csv/report_ratiosMaxRegions.csv")
@@ -19,10 +25,21 @@ library(ggplot2)
 
 #Compare distribution of Dyncov of SCoPs and parents
 compDyncovScopParent <- ggplot(report_ratiosParents, aes(factor(is_parent), dyncov)) +
-  geom_violin(aes(fill=factor(is_parent))) +
-  theme(legend.position="bottom")
+  geom_violin(aes(fill=factor(is_parent)), draw_quantiles = c(0.25, 0.5, 0.75)) +
+  labs(y = 'DynCov'[p]~'/DynCov'[s]) +
+  scale_y_continuous(label=function(x){return(paste("", x, "%"))})+
+  theme(legend.position="bottom")+
+  defaultSettings
+
+#Plot invalidReasons
+pieInvalidReasons <- ggplot(report_invalidReasons, aes(x="", y=occurrence, fill=invalid_reason)) +
+  geom_bar(width=1, stat="identity")+coord_polar("y", start=0) +
+  defaultSettings
+
+#If the script reaches to this point export all plots
 svg(filename = "../svg/compDyncovScopParent.svg")
 compDyncovScopParent
 dev.off()
-
-#Plot invalidReasons
+svg(filename = "../svg/pieInvalidReasons.svg")
+pieInvalidReasons
+dev.off()
